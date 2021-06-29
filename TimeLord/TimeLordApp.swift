0143b4010b831bf52d19b67dab334f479fec2e87
ohlulu <z30262226@gmail.com>
2021-06-29 22:01:41 +0800

    [ chore ] : init porject

---
+++ TimeLord/TimeLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLordApp.swift
+//  TimeLord
+//
+//  Created by Ohlulu on 2021/6/29.
+//
+
+import SwiftUI
+
+@main
+struct TimeLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
