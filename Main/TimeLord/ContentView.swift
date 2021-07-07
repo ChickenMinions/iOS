ce155699285c0c37c4525c12b4d166fd1336faca
ohlulu <z30262226@gmail.com>
2021-07-07 21:36:27 +0800

    [ chore ] : create workspace

---
+++ Main/TimeLord/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeLord
+//
+//  Created by Ohlulu on 2021/6/29.
+//
+
+import SwiftUI
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
+
+struct RootView: View {
+
+    var body: some View {
+        Text("Lo")
+    }
+}
--- Main/TimeLord/TimeLordApp.swift
+++ Main/TimeLord/TimeLordApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TimeLordApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
 }
